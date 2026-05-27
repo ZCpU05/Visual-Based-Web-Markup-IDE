27a4dc1bae1f49efe15aef63a78da1c1c074003a
ZW79 <ztwalker05@gmail.com>
2026-05-27 14:21:46 +0100

    Initial Commit for setting up project

---
+++ VWIDE/VWIDE/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VWIDE
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
