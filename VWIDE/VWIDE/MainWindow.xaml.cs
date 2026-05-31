0511013f3a5b7e92d2db77f93ffd8abdf6e67e5f
ZW79 <ztwalker05@gmail.com>
2026-05-31 12:11:56 +0100

    Layout and file opening functionality

--- VWIDE/VWIDE/MainWindow.xaml.cs
+++ VWIDE/VWIDE/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using ICSharpCode.AvalonEdit;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,46 @@ namespace VWIDE
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool phpEnabled = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void openMenuItem_Click(object sender, RoutedEventArgs e) //runs when open button is clicked from file
+        {
+            getFile();
+        }
+        private void saveMenuItem_Click(object sender, RoutedEventArgs e) //runs when save button is clicked from file
+        {
+            System.Windows.MessageBox.Show("Saving file");
+        }
+        private void getFile() //calls an open file dialouge and gets the selected file
+        {
+            string fileContent = string.Empty;
+            string filePath = string.Empty;
+
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            if (!phpEnabled)
+            {
+                openFileDialog.Filter = "html files (*.html)|*.html|All files (*.*)|*.*";
+            }
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath = openFileDialog.FileName;
+
+                using (Stream stream = openFileDialog.OpenFile())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    fileContent = reader.ReadToEnd();
+                }
+            }
+
+            textEditor.Text = fileContent; //Writes the file contents to the text editor
         }
     }
 }
